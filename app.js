aca6871d3bf7092c23f5b5227f667836d19594a9
saroyar <yarsaro2001@gmail.com>
2022-06-23 16:46:33 +0530

    login works fine

--- app.js
+++ app.js
@@ -50,7 +50,36 @@ app.post('/register',async(req,res)=>{
     } catch (error) {
         console.log(`error occure in creating collection ${error}`);
     }
-})
+});
+
+app.post("/login",async(req,res)=>{
+    try {
+        
+        const {email,password}=req.body;
+        if(!(email&&password))
+          res.status(400).send("send all credential");
+
+        const user=await User.findOne({email});
+
+        if(user&&(await bcrypt.compare(password,user.password))){
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                  expiresIn: "2h",
+                }
+              );
+
+              user.token=token;
+
+              res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 // Logic goes here
 
